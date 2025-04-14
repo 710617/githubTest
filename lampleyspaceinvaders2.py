369cb9e7a63964787944976742aff2b195922d3e
710617 <clampley710617@muhsdstudents.org>
2025-04-14 08:40:49 -0700

    Add files via upload

--- lampleyspaceinvaders2.py
+++ lampleyspaceinvaders2.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import *
+from pygame.locals import*
 
 #define fps
 clock = pygame.time.Clock()
@@ -7,10 +7,15 @@ fps = 60
 
 
 screen_width = 600
-screen_height = 600
+screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Space Invanders')
+
+
+#define colours
+red = (255, 0, 0)
+green = (0, 255, 0)
 
 
 #load image
@@ -22,39 +27,71 @@ def draw_bg():
 
 #create spaceship class
 class Spaceship(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, health):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("img/spaceship.png")
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.health_start = health
+        self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()
 
 
     def update(self):
         #set movement speed
         speed = 8
+        #set a cooldown variable
+        cooldown = 500 #milliseconds
 
         #get key press
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= speed
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
-            self. rect.x += speed
+            self.rect.x += speed
+
+        #record current time
+        time_now = pygame.time.get_ticks()
+        #shoot
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+
+        #draw health bar
+        pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
+        if self.health_remaining > 0:
+            pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)),15))
+
+#create Bullets class
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y,):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/Bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+       
+
+
+
+    def update(self):
+        self.rect.y -= 5
 
 
 #create sprite groups
 spaceship_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
 
 
 #create player
-spaceship = Spaceship(int(screen_width / 2), screen_height - 100)
+spaceship = Spaceship(int(screen_width / 2), screen_height - 100, 3)
 spaceship_group.add(spaceship)
 
 
 
-run =True
+run = True
 while run:
 
-    clock.tick(fps)
+    clock.tick(fps)   
 
     #draw background
     draw_bg()
@@ -65,15 +102,19 @@ while run:
             run = False
 
 
-
-            #update spaceship
-            spaceship.update()
-
-
-            #draw sprite group
-            spaceship_group.draw(screen)
+    #update spaceship
+    spaceship.update()
 
 
-            pygame.display.update()
+    #update sprite group
+    bullet_group.update()
+
+    #draw sprite group
+    spaceship_group.draw(screen)
+    bullet_group.draw(screen)
+
+
+    pygame.display.update()
+
 
 pygame.quit()
